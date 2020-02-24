beef81ea9a028eefcf3bfc414cce3d18b4beeea1
lichuanqi <lc@dlc618.com>
2020-02-24 14:38:18 +0800

    增加 视频转图片

--- preprocessing/zhongzhi_zishiying.py
+++ preprocessing/zhongzhi_zishiying.py
@@ -1,19 +1,20 @@
+'''
+@Description: 
+@Autor: lichuan
+@Email: lc@dlc618.com
+@LastEditTime: 2020-02-24 14:37:01
+'''
 import cv2
 import numpy as np
 
 
-img = cv2.imread('/media/lc/8A986A3C986A26C3/model/data/m4.png')
+img = cv2.imread('/media/lc/Data/model/data/450.jpg')
 x, y, n = img.shape
 
 # 参数设置
 min_size = int(1)         # 滤波窗口的最小尺寸，一般为奇数
 max_size = int(7)         # 滤波窗口的最大尺寸，一般为奇数
-pading = int((max_size - 1) / 2)　  # 根据最大滤波窗口尺寸给图像添加边界
-
-
-
- 
-
+pading = int((max_size-1)/2) # 根据最大滤波窗口尺寸给图像添加边界
 
 # 边界0填充,倒影填充
 img_pading = cv2.copyMakeBorder(img,pading,pading,pading,pading,cv2.BORDER_DEFAULT,value=0)
---
+++ video_to_image.py
@@ -0,0 +1,65 @@
+'''
+@Description: 读取视频并保存指定图像帧
+@Autor: lichuan
+@Email: lc@dlc618.com
+@LastEditTime: 2020-02-24 14:34:16
+'''
+
+import cv2
+import time
+
+
+# 相关函数
+def resize_by_width(image,width_new):
+    '''
+    @description: 按照给定的宽度对图片进行所需比例的缩放操作
+    @param {image}:读取后的图片
+    @param {width_new}:想要得到的图片宽度
+    @return: 处理后的图片
+    '''
+    x, y, z= image.shape
+    rate = width_new / y
+    # resize函数输入宽度在前，高度在后 
+    size = (int(y*rate), int(x*rate))
+    img_new = cv2.resize(image, size, interpolation=cv2.INTER_CUBIC)  
+    
+    return img_new
+
+
+# 视频路径并读取
+path = '/media/lc/Data/modle_and_code/data/video/30s.mp4'
+cap = cv2.VideoCapture(path)
+
+# 输出保存相关数据
+# save_frame_no = []
+save_frame_no = [350,380,410,450]
+save_path = '/media/lc/Data/modle_and_code/data/'
+
+# 参数赋值
+i = 1                            # 视频帧计数器
+time_start = time.time()         # 获取循环开始时间
+
+while cap.isOpened():
+    # 逐帧读取图片
+    ret, frame = cap.read()
+
+    # 判断视频是否结束
+    if ret == False:
+        time_used = time.time() - time_start
+        print('视频结束，总处理时间为 %f !'%time_used)
+        break
+    print('第 %d 张图片读取成功，正在处理中... ...'%i)
+
+    # 修改尺寸
+    
+    frame = resize_by_width(frame,1920)
+
+    # 图像数据保存到本地
+    if i in save_frame_no:
+        name_yuantu = save_path + str(i) + '.png'
+        cv2.imwrite(name_yuantu, frame)
+        print('----第 %d 帧图像保存到本地！'%i)
+
+    i += 1
+
+
