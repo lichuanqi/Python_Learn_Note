83e54100a8305c43eda611f49375d81896c5ff57
lichuanqi <lc@dlc618.com>
2020-05-20 12:33:35 +0800

    读取文件夹内文件名称并写入txt文件

--- get_filename.py
+++ get_filename.py
@@ -15,6 +15,11 @@ file_dir = '/media/lc/Data/modle_and_code/新建文件夹/'
 file_name = []
 
 def get_file_name(file_dir):
+    """
+    获取指定文件夹内的文件名称
+    file_dir：文件夹地址
+    file_name：读取到的文件绝对地址
+    """
     
     file_name = []
 
@@ -23,4 +28,23 @@ def get_file_name(file_dir):
             file_name.append(root + file)
         return file_name
         
-print(get_file_name(file_dir))
+
+def write_file_name(file_name, txt_name):
+    """
+    将读取到的文件名称写入txt文件中
+    file_name:读取到的文件名称
+    txt_name：输出文件地址
+    """
+    
+    for name in file_name:
+        with open (txt_name, 'a') as f:
+            f.write(name + '\n')
+
+
+if __name__ == "__main__":
+
+    file_dir = '/media/lc/Data/modle_and_code/data/image/'
+    txt_name = '/media/lc/Data/modle_and_code/env_linux/yolov4/images.txt'
+
+    file_name = get_file_name(file_dir)
+    write_file_name(file_name, txt_name)
