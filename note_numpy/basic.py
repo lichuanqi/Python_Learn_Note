726875c562a6dea49bc9f1c2673c3a0dc4a538b6
“lichuanqi” <lc@dlc618.com>
2021-05-22 20:08:34 +0800

    新增：矩阵基础知识

---
+++ note_numpy/basic.py
@@ -0,0 +1,15 @@
+###################################
+# 矩阵一一配对
+# lichuan
+# lc@dlc618.com
+###################################
+
+import numpy as np
+import random as ra
+
+x = [1,2,3,4,5]
+y = [7,8,9,9,10]
+result = []
+
+for i in range(len(x)):
+    print([x[i],y[i]])
