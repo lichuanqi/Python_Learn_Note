67ab834f1da73f86b286acc46c819601a32b7f80
“lichuanqi” <lc@dlc618.com>
2021-07-15 13:06:17 +0800

    新增： * 二值化图像中连通域的轮廓和内部填充

--- note_cv2/contour_draw.py
+++ note_cv2/contour_draw.py
@@ -1,5 +1,5 @@
 #==============================================
-# 提取二值化图像中的连通域轮廓信息
+# 提取二值化图像中连通域的轮廓信息
 # lichuan
 # lc@dlc618.com
 # 2021.7.2
@@ -16,7 +16,15 @@ if __name__ == '__main__':
     mask = mask * 255
 
     ret, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
+    
+    img_fill = img.copy()
+    for i in range(len(contours)):
+        # 轮廓
+        # cv2.drawContours(img, contours[i], -1, (0, 0, 255), 3)
+        # 内部填充
+        cv2.fillConvexPoly(img_fill, contours[i], (0, 0, 255))
 
-    cv2.imshow('img',img)
+    img_viz = cv2.addWeighted(img, 0.6, img_fill, 0.4, 0)
+
+    cv2.imshow('img',img_viz)
     cv2.waitKey(0)
