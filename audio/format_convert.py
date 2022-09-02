1599125028bd4aba8cf4c605c11644610a55f8f4
lichuanqi <lc@dlc618.com>
2022-09-02 15:34:32 +0800

    新增： * 使用ffmpeg将m4a转换为wav格式

--- audio/format_convert.py
+++ audio/format_convert.py
@@ -4,14 +4,35 @@ import os
 import subprocess
 
 
-file_path = "D:/Code/ASRT_SpeechRecognition_V1.3.0/data/20220829_091159.m4a"
-
-if 'wav' not in file_path:
-    f = file_path.split('/')[-1].split('.')[-1]
-    save_path = file_path.replace(f, 'wav')
+def m4a2wav(input, output):
 
     cmd_head = 'ffmpeg -i '
-    cmd = cmd_head + file_path + ' -ar 16000 ' + save_path
-
-    print('开始执行以下命令：\n', cmd)
+    cmd = cmd_head + input + ' -ar 16000 ' + output
     subprocess.Popen(cmd, shell=True, stdout=None, stderr=None).wait()
+
+
+path = 'D:/Data/Jiyao_address/002/'
+# path = "D:/Code/ASRT_SpeechRecognition_V1.3.0/data/20220829_091159.m4a"
+
+save_path = ''
+
+if not save_path:
+    save_path = path
+
+# 判断文件还是文件夹
+if path.endswith('.m4a'):
+    files = [path]
+else:
+    files = [path + i for i in os.listdir(path)]
+
+# 判断文件格式
+for file in files:
+    if 'wav' not in file:
+
+        name = file.split('/')[-1].split('.')[0]
+        save_name = os.path.join(save_path, name + '.wav')  
+
+        m4a2wav(file, save_name)
+
+        print('', name, '  ->  ', save_name)
+        print('--Over')
