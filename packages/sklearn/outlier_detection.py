9c07db41645d8783897e11f964882079fd6ed594
lichuanqi <lc@dlc618.com>
2022-12-08 17:27:22 +0800

    修改: - LOF算法检测离群点时通过计算所有点的LOF分定量评价点的离散程度

--- packages/sklearn/outlier_detection.py
+++ packages/sklearn/outlier_detection.py
@@ -6,15 +6,14 @@ from sklearn.neighbors import LocalOutlierFactor
 
 
 if __name__ == '__main__':
-    data = np.array([[0,0], [1,2], [2,3], [3,3]])
+    data = np.array([[0,0], [1,2], [2,3], [3,3], [20,20]])
     point = np.array([[1,1]])
 
     data_std = data.std()
-    print(data_std)
 
     clf = LocalOutlierFactor(n_neighbors=2)
     predict = clf.fit_predict(data)
     score = clf.negative_outlier_factor_
 
-    print(predict)
-    print(score)
+    print(f'训练集所有数据点的离群结果为: {predict}')
+    print(f'训练集所有数据点的LOF分为: {score.sum()}')
