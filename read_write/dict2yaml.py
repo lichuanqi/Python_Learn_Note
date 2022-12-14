cb3bb4471d3a335c08c168714a97a148c6f99297
lichuanqi <lc@dlc618.com>
2022-12-14 13:07:36 +0800

    新增: 字典dict与yaml文件之间的保存和读取

--- read_write/dict2yaml.py
+++ read_write/dict2yaml.py
@@ -1,16 +1,35 @@
-
-# 
-
+"""字典dict与yaml文件之间的保存和读取"""
 import yaml
 
-a = [['name','lc'], ['age', 18.01234]]
-b = {}
 
-for i in a:
-    b[i[0]] = i[1]
+def dict2yaml(dicts:dict, savename:str) -> None:
+    """字典保存为yaml文件
+    Args
+        dicts (dict)   : 要保存的字典数据
+        savename (str) : 文件保存名称
+    Output
+        savename.yaml
+    """
+    with open(savename, 'w', encoding='utf-8') as f:
+        f.write(yaml.dump(dicts, allow_unicode=True))
 
-b['new'] = '%.4f'%(0.2345677)
-save_name = 'dict.yaml'
 
-with open(save_name, 'w') as f:
-    f.write(yaml.dump(b, allow_unicode=True))
+def yaml2dict(savename:str) -> dict:
+    """yaml文件读取为字典"""
+    with open(savename, 'r', encoding='utf-8') as f:
+        data = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+    return data
+
+
+if __name__=='__main__':
+
+    data = {'1': {'速递': '11'}, '2': '2频'}
+    savename = 'dataset/dict_yaml.yaml'
+    
+    # 保存
+    dict2yaml(data, savename)
+
+    # 读取
+    data = yaml2dict(savename)
+    print(data)
