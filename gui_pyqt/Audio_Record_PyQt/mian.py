dba1218705208ec93cdb07717a0d8ee68f531f7e
lichuanqi <lc@dlc618.com>
2023-01-05 16:59:34 +0800

    新增： - 参数下拉选框设置默认值 - 在状态栏显示录音时间

--- gui_pyqt/Audio_Record_PyQt/mian.py
+++ gui_pyqt/Audio_Record_PyQt/mian.py
@@ -11,7 +11,7 @@ from PyQt5.QtWidgets import QPushButton, QTableWidget, QTableWidgetItem
 from PyQt5.QtWidgets import QLabel, QTextEdit, QLineEdit, QComboBox
 from PyQt5.QtWidgets import QMessageBox, QFileDialog, QStatusBar
 
-from PyQt5.QtMultimedia import QAudioRecorder, QAudioEncoderSettings
+from PyQt5.QtMultimedia import QAudioRecorder, QAudioFormat
 
 
 class RecordWindow(QMainWindow):
@@ -20,11 +20,11 @@ class RecordWindow(QMainWindow):
         super().__init__()
 
         self.audioRecorder = QAudioRecorder()
+        self.audioRecorder.durationChanged.connect(self.update_recorder_time)
 
         self.init_ui()
         self.init_params()
 
-        
     def init_ui(self):
         """中间主要区域"""
 
@@ -63,6 +63,7 @@ class RecordWindow(QMainWindow):
         la_sample_rate = QLabel('采 样 率:', self)
         self.cb_sample_rate = QComboBox()
         self.cb_sample_rate.setMinimumWidth(700)
+        self.cb_sample_rate.currentTextChanged.connect(self.update_sample_rate)
         lo_params_sample_rate.addWidget(la_sample_rate)
         lo_params_sample_rate.addWidget(self.cb_sample_rate)
         lo_params_sample_rate.addStretch()
@@ -83,11 +84,22 @@ class RecordWindow(QMainWindow):
         lo_params_container.addWidget(self.cb_container)
         lo_params_container.addStretch()
 
+        lo_params_savepath = QHBoxLayout()
+        la_params_container = QLabel('保存路径:', self)
+        self.cb_savepath = QLineEdit()
+        self.cb_savepath.setReadOnly(True)
+        self.cb_savepath.setFixedWidth(700)
+        self.cb_savepath.setPlaceholderText('请点击开始选择保存路径和文件名')
+        lo_params_savepath.addWidget(la_params_container)
+        lo_params_savepath.addWidget(self.cb_savepath)
+        lo_params_savepath.addStretch()
+
         lo_params.addLayout(lo_params_device)
         lo_params.addLayout(lo_params_codec)
         lo_params.addLayout(lo_params_sample_rate)
         lo_params.addLayout(lo_params_channal)
         lo_params.addLayout(lo_params_container)
+        lo_params.addLayout(lo_params_savepath)
         layout.addLayout(lo_params)
         layout.addSpacing(30)
 
@@ -100,8 +112,6 @@ class RecordWindow(QMainWindow):
         lo_control.addWidget(bt_end)
         lo_control.addStretch()
         layout.addLayout(lo_control)
-
-        # 底部弹簧
         layout.addStretch()
 
         widget = QWidget()
@@ -113,9 +123,8 @@ class RecordWindow(QMainWindow):
         self.status_bar.showMessage('初始化完成')
         self.setStatusBar(self.status_bar)
 
-
     def init_params(self):
-        # 可选项
+        # 参数可选项
         self.cb_device.addItems(self.audioRecorder.audioInputs()) # 录音设备
         self.cb_codec.addItems(self.audioRecorder.supportedAudioCodecs()) # 编码方式
         samplerates, _ = self.audioRecorder.supportedAudioSampleRates() # 采样率 
@@ -124,26 +133,27 @@ class RecordWindow(QMainWindow):
         self.cb_channal.addItems(['1', '2'])
         self.cb_container.addItems(self.audioRecorder.supportedContainers()) # 视频格式
         
-        # 设置默认值
-        self.cb_sample_rate.setCurrentText('16000')
+        # 设置参数默认值
+        default_codec = self.audioRecorder.audioSettings().codec()
+        default_sample_rate = self.audioRecorder.audioSettings().sampleRate()
+        default_channal = self.audioRecorder.audioSettings().channelCount()
+        default_container = self.audioRecorder.containerFormat()
 
+        self.cb_codec.setCurrentText(default_codec)
+        self.cb_sample_rate.setCurrentText(str(default_sample_rate))
+        self.cb_channal.setCurrentText(str(default_channal))
+        self.cb_container.setCurrentText(default_container)
 
     def start_(self):
         """录音 开始/结束"""
         if self.audioRecorder.state() == 0:
-            self.audioRecorder.audioSettings().setSampleRate(16000)
-            self.audioRecorder.audioSettings().setChannelCount(2)
-
+            # 选择文件保存名称
             savename, pathtype = QFileDialog.getSaveFileName(self, "文件保存", 
                     f"gui_pyqt/Audio_Record_PyQt/audio.wav" ,'wav(*.wav)')
             self.audioRecorder.setOutputLocation(QUrl.fromLocalFile(savename))
+            self.cb_savepath.setText(savename)
 
-            print(self.audioRecorder.audioSettings().codec())
-            print(self.audioRecorder.audioSettings().sampleRate())
-            print(self.audioRecorder.audioSettings().channelCount())
-            print(self.audioRecorder.containerFormat())
-
-            # self.audioRecorder.record()
+            self.audioRecorder.record()
             print('开始录音')
 
         elif self.audioRecorder.state() == 1:
@@ -154,10 +164,25 @@ class RecordWindow(QMainWindow):
             self.audioRecorder.stop()
             print('录音暂停中, 已停止')
 
-
     def pause_(self):
         """录音 暂停/继续"""
         self.audioRecorder.pause()
+
+    def update_sample_rate(self):
+        """采样率的下拉选框变化时更新参数"""
+        sample_rate_new = self.cb_sample_rate.currentText()
+
+        audio_format = QAudioFormat()
+        audio_format.setSampleRate(int(sample_rate_new))
+
+        if self.audioRecorder.state() == 0:
+            print(f'采样率发生变化: {sample_rate_new}')
+            print(self.audioRecorder.audioSettings().sampleRate())
+
+    def update_recorder_time(self):
+        """在状态栏显示录音时间"""
+        duration = self.audioRecorder.duration()
+        self.status_bar.showMessage(f'时间: {duration/1000}s')
 
 
 if __name__ == '__main__':
