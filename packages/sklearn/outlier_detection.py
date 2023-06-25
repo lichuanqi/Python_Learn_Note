6cd0e5d21b6fb5a9825472dfe5e0691adf3a4875
lichuanqi <lc@dlc618.com>
2023-06-25 18:24:01 +0800

    增加根据LOF异常值检测结果绘制散点图

--- packages/sklearn/outlier_detection.py
+++ packages/sklearn/outlier_detection.py
@@ -2,6 +2,7 @@
 离群点检测
 """
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.neighbors import LocalOutlierFactor
 
 
@@ -11,9 +12,15 @@ if __name__ == '__main__':
 
     data_std = data.std()
 
+    # 筛选异常值
     clf = LocalOutlierFactor(n_neighbors=2)
     predict = clf.fit_predict(data)
     score = clf.negative_outlier_factor_
 
     print(f'训练集所有数据点的离群结果为: {predict}')
     print(f'训练集所有数据点的LOF分为: {score.sum()}')
+
+    # 绘制散点图
+    plt.scatter(data[:,0], data[:,1], s=20, c=predict, marker='o')
+    plt.colorbar()
+    plt.show()
