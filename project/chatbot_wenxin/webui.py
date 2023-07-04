88279bfb6ce117ae51dfca2bcb4f60f8a5cdf368
lichuanqi <lc@dlc618.com>
2023-07-04 18:02:37 +0800

    左侧增加模型选择、上传知识库文件、模型参数配置

--- project/chatbot_wenxin/webui.py
+++ project/chatbot_wenxin/webui.py
@@ -7,6 +7,12 @@ from api import ACCESS_TIME, ACCESS_TOKEN
 from api import update_access_token, chat_by_token
 
 
+llm_model_dict = {'baidu-wenxin': '',
+                  'xunfei-xinghuo': ''}
+embedding_model_dict = {"text2vec-base": '',
+                        "m3e": ''}
+
+
 def predict_base(message, chatbot, history:list=[]):
     """文本生成主函数, 主要用户调试每次都返回固定的字符串
     
@@ -53,39 +59,86 @@ def on_clear():
     return [], None
 
 
-with gr.Blocks(
-    title="LLM",
-    theme=gr.themes.Soft()
-) as demo:
-    gr.Markdown(
-        """
-        ## 🚀LLM助手
-        使用文心一言商用API实现的大语言模型助手        
-        """
-    )
+with gr.Blocks(title="LLM",
+               theme=gr.themes.Soft()) as demo:
+    gr.Markdown("""
+                ## 🚀LLM助手
+                使用文心一言商用API实现的大语言模型助手        
+                """)
     history = []
     state = gr.State([])
-    chatbot = gr.Chatbot().style(height=450)
-    msg = gr.Textbox(
-        label="Chat Message Box",
-        placeholder="请输入要提问的问题",
-        show_label=False,
-    ).style(container=False)
-    gr.Examples(
-        examples=["写一个大学生心理情景剧大赛的参赛剧本，描述大学生活中，少年A从迷茫到找到奋斗方向的故事",
-                  "帮我设计一个可以打出sin函数的代码",
-                  "查一个知识："],
-        inputs=msg,
-        outputs=None,
-        cache_examples=False,
-        label="快速提问"
-    )
 
-    with gr.Column():
-        with gr.Row():
-            submit = gr.Button("Submit")
-            stop = gr.Button("Stop")
-            clear = gr.Button("Clear")
+    with gr.Row():
+        with gr.Column(scale=1):
+            with gr.Accordion("模型选择"):
+                large_language_model = gr.Dropdown(
+                    list(llm_model_dict.keys()),
+                    label="large language model",
+                    value=list(list(llm_model_dict.keys()))[0])
+                embedding_model = gr.Dropdown(
+                    list(embedding_model_dict.keys()),
+                    label="Embedding model",
+                    value=list(embedding_model_dict.keys())[0])
+            
+            with gr.Accordion("上传知识库文件"):
+                upload_file = gr.File(
+                    label='上传知识库文件',
+                    file_types=['.txt', '.md', '.docx'])
+
+            with gr.Accordion("模型参数配置"):
+                use_web = gr.Radio(["True", "False"],
+                                   label="Web Search",
+                                   value="False")
+                VECTOR_SEARCH_TOP_K = gr.Slider(
+                    1,
+                    10,
+                    value=6,
+                    step=1,
+                    label="vector search top k",
+                    interactive=True)
+                HISTORY_LEN = gr.Slider(
+                    0,
+                    3,
+                    value=0,
+                    step=1,
+                    label="history len",
+                    interactive=True)
+                temperature = gr.Slider(
+                    0,
+                    1,
+                    value=0.01,
+                    step=0.01,
+                    label="temperature",
+                    interactive=True)
+                top_p = gr.Slider(
+                    0,
+                    1,
+                    value=0.9,
+                    step=0.1,
+                    label="top_p",
+                    interactive=True)
+
+        with gr.Column(scale=4):
+            chatbot = gr.Chatbot().style(height=450)
+            msg = gr.Textbox(
+                label="Chat Message Box",
+                placeholder="请输入要提问的问题",
+                show_label=False).style(container=False)
+            
+            gr.Examples(
+                examples=["写一个大学生心理情景剧大赛的参赛剧本，描述大学生活中，少年A从迷茫到找到奋斗方向的故事",
+                        "帮我设计一个可以打出sin函数的代码",
+                        "查一个知识："],
+                inputs=msg,
+                outputs=None,
+                cache_examples=False,
+                label="快速提问")
+
+            with gr.Column():
+                with gr.Row():
+                    submit = gr.Button("Submit")
+                    stop = gr.Button("Stop")
+                    clear = gr.Button("Clear")
     
     # 输入框回车槽函数
     msg_event = msg.submit(
