ab78bd14f78bff1135fdf19fc29b648bf1de08a5
lichuanqi <lc@dlc618.com>
2023-09-05 09:27:54 +0800

    增加：scipy.stats.kstest KS假设检验

--- packages/scipy/stats.py
+++ packages/scipy/stats.py
@@ -1,8 +1,39 @@
+from pprint import pprint
 import numpy as np
-from scipy.stats import pearsonr, spearmanr
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import normalize
+from scipy.stats import pearsonr, spearmanr, kstest, uniform, norm
+from scipy.stats import describe
+
+matplotlib.use('TkAgg')
+
 
 x = [1.1,1,2.3,3,3,4,5,5,6,6]
 y = [5,5,7,6.7,7,7.5,8,8,8,8]
+
+
+def test_ks():
+    # np.random生成随机数
+    # x = np.random.uniform(low=5, high=10, size=20)
+    # x = np.random.normal(0, 0.5, 200)
+
+    # scipy.stats生成随机数
+    # x = uniform.rvs(loc=0, scale=10, size=100, random_state=np.random.default_rng())
+    x = norm.rvs(loc=0, scale=0.5, size=200)
+
+    print(type(x))
+    print(x)
+    pprint(describe(x))
+
+    # 频数直方图
+    count, bins, ignored = plt.hist(x, 15, density=True)
+    plt.show()
+
+    # ks检验
+    u, std = np.mean(x), np.std(x)
+    sta, pvalue = kstest(x, norm.cdf, args=(u, std))
+    print(sta, pvalue)
 
 
 def test_pearsonr():
@@ -33,5 +64,6 @@ def test_spearmanr():
     print("spearmanr corr_coef: %s, p_value: %s"%(corr_coef, p_value))
 
 
-test_pearsonr()
-test_spearmanr()
+test_ks()
+# test_pearsonr()
+# test_spearmanr()
