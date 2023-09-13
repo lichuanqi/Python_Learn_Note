e69925b5fcde0dc56834f470a91dfeebf65151bc
lichuanqi <lc@dlc618.com>
2023-09-13 13:40:27 +0800

    np.arange和np.linspace

--- packages/numpy/basic.py
+++ packages/numpy/basic.py
@@ -9,7 +9,23 @@ import random as ra
 
 x = [1,2,3,4,5]
 y = [7,8,9,9,10]
+
+xy = np.array([
+    [1,2,3,4,5],
+    [7,8,9,9,10]])
 result = []
 
 for i in range(len(x)):
     print([x[i],y[i]])
+
+print(xy.shape)
+for i in range(xy.shape[1]):
+    for j in range(xy.shape[0]):
+        print(i, j, xy[j, i])
+print(xy[1,2])
+
+
+x1 = np.arange(0, 15, 1)
+x2 = np.linspace(0, 15, 15, endpoint=False)
+print(x1)
+print(x2)
